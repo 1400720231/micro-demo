ee751bfab0e3b64613186df62f49c49981576d90
xiongyao <18778331181@sina.cn>
2022-04-16 23:34:38 +0800

    fist

---
+++ user/handler/user.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+	"context"
+
+	log "github.com/micro/micro/v3/service/logger"
+
+	user "user-srv/proto"
+)
+
+type User struct{}
+
+// Return a new handler
+func New() *User {
+	return &User{}
+}
+
+// Call is a single request handler called via client.Call or the generated client code
+//curl "http://localhost:8080/user/Call"
+func (e *User) Call(ctx context.Context, req *user.Request, rsp *user.Response) error {
+	log.Info("Received User.Call request")
+	rsp.Msg = "Hello " + req.Name
+	return nil
+}
+
+// XiongYao is a single request handler called via client.Call or the generated client code
+//curl "http://localhost:8080/user/UserInfo"
+func (e *User) UserInfo(ctx context.Context, req *user.Request, rsp *user.Response) error {
+	log.Info("Received User.UserInfo request")
+	rsp.Msg = req.Name + "是大帅比"
+	return nil
+}
