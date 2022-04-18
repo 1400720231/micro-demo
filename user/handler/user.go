b99c0a0325a71fc86e7a7f311a3ca7e1b6c479ce
xiongyao <18778331181@sina.cn>
2022-04-18 16:48:02 +0800

    修改gitignore

--- user/handler/user.go
+++ user/handler/user.go
@@ -24,7 +24,8 @@ func (e *User) Call(ctx context.Context, req *user.Request, rsp *user.Response) 
 }
 
 // XiongYao is a single request handler called via client.Call or the generated client code
-//curl "http://localhost:8080/user/UserInfo"
+// curl "http://localhost:8080/user/UserInfo"
+// 当访问http://localhost:8080/user/UserInfo的时候调用这个处理函数
 func (e *User) UserInfo(ctx context.Context, req *user.Request, rsp *user.Response) error {
 	log.Info("Received User.UserInfo request")
 	rsp.Msg = req.Name + "是大帅比"
